f5c971b932b50717a5bf1e19f0e73ca2cfa49e03
Arne Stappen <meh@agoat.de>
2016-11-26 16:07:19 +0100

    Adjustments for Contao 4.3 integration

--- src/Resources/contao/classes/AutoCombiner.php
+++ src/Resources/contao/classes/AutoCombiner.php
@@ -34,8 +34,10 @@ use Agoat\AutoPrefixer\AutoPrefixer;
 class AutoCombiner extends Combiner
 {
 
+	protected $autoprefixer = null;
+	
 	/**
-	 * Generate the combined file and add vendor prefixes with autoprefixer class
+	 * Generate the combined file, add vendor prefixes with autoprefixer and return its path
 	 *
 	 * @param string $strUrl An optional URL to prepend
 	 *
@@ -43,77 +45,135 @@ class AutoCombiner extends Combiner
 	 */
 	public function getCombinedFile($strUrl=null)
 	{
-		// PageModel needed
-		if (!isset($GLOBALS['objPage']))
+		// Prepare autoprefixer
+		if ($GLOBALS['objPage'])
 		{
-			return;
+			$objLayout = \LayoutModel::findByPk($GLOBALS['objPage']->layoutId);
+
+			// prepare browser list
+			$browsers = explode(',', $objLayout->browsers);
+			array_walk($browsers, function (&$value) { $value = trim(html_entity_decode($value)); });
+
+			$this->autoprefixer = new AutoPrefixer($browsers);
 		}
-		$objLayout = $GLOBALS['objPage']->getRelated('layout');
-		
-		// prepare browser list
-		$browsers = explode(',', $objLayout->browsers);
-		array_walk($browsers, function (&$value) { $value = trim(html_entity_decode($value)); });
 
-		$autoprefixer = new AutoPrefixer($browsers);
+		if (\Config::get('debugMode'))
+		{
+			return $this->getDebugMarkup();
+		}
 
-		
+		return $this->getCombinedFileUrl($strUrl);
+	}
+
+
+	/**
+	 * Generates the debug markup.
+	 *
+	 * @return string The debug markup
+	 */
+	protected function getDebugMarkup()
+	{
+		$return = $this->getFileUrls();
+
+		if ($this->strMode == self::JS)
+		{
+			return implode('"></script><script src="', $return);
+		}
+		else
+		{
+			return implode('"><link rel="stylesheet" href="', $return);
+		}
+	}
+
+	/**
+	 * Generates the files, add vendor prefixes and returns the URLs.
+	 *
+	 * @return array The file URLs
+	 */
+	public function getFileUrls()
+	{
+		$return = array();
+		$strTarget = substr($this->strMode, 1);
+
+		foreach ($this->arrFiles as $arrFile)
+		{
+			$content = file_get_contents(TL_ROOT . '/' . $arrFile['name']);
+
+			// Compile SCSS/LESS files into temporary files
+			if ($arrFile['extension'] == self::SCSS || $arrFile['extension'] == self::LESS)
+			{
+				$strPath = 'assets/' . $strTarget . '/' . str_replace('/', '_', $arrFile['name']) . $this->strMode;
+
+				$objFile = new \File($strPath);
+				if ($this->autoprefixer !== null)
+				{
+					$objFile->write($this->autoprefixer->rewrite($this->handleScssLess($content, $arrFile)));
+				}
+				else
+				{
+					$objFile->write($this->handleScssLess($content, $arrFile));
+				}
+				$objFile->close();
+
+				$return[] = $strPath;
+			}
+			// Compile CSS files into temporary files
+			else if ($arrFile['extension'] == self::CSS)
+			{
+				$strPath = 'assets/' . $strTarget . '/' . str_replace('/', '_', $arrFile['name']) . $this->strMode;
+
+				$objFile = new \File($strPath);
+				if ($this->autoprefixer !== null && strpos($arrFile['name'], 'assets/contao') === false) // exclude framework css
+				{
+					$objFile->write($this->autoprefixer->rewrite($this->handleCss($content, $arrFile)));
+				}
+				else
+				{
+					$objFile->write($this->handleCss($content, $arrFile));
+				}
+				$objFile->close();
+
+				$return[] = $strPath;
+			}
+			else
+			{
+				$name = $arrFile['name'];
+
+				// Strip the web/ prefix (see #328)
+				if (strncmp($name, 'web/', 4) === 0)
+				{
+					$name = substr($name, 4);
+				}
+
+				// Add the media query (see #7070)
+				if ($arrFile['media'] != '' && $arrFile['media'] != 'all' && strpos($content, '@media') === false)
+				{
+					$name .= '" media="' . $arrFile['media'];
+				}
+
+				$return[] = $name;
+			}
+		}
+
+		return $return;
+	}
+
+	/**
+	 * Generate the combined file, add vendor prefixes and return its path
+	 *
+	 * @param string $strUrl An optional URL to prepend
+	 *
+	 * @return string The path to the combined file
+	 */
+	protected function getCombinedFileUrl($strUrl=null)
+	{
 		if ($strUrl === null)
 		{
 			$strUrl = TL_ASSETS_URL;
 		}
 
-
 		$strTarget = substr($this->strMode, 1);
 		$strKey = substr(md5($this->strKey), 0, 12);
-
-		// Do not combine the files in debug mode (see #6450)
-		if (\Config::get('debugMode'))
-		{
-			$return = array();
-
-			foreach ($this->arrFiles as $arrFile)
-			{
-				$content = file_get_contents(TL_ROOT . '/' . $arrFile['name']);
-
-				// Compile SCSS/LESS files into temporary files
-				if ($arrFile['extension'] == self::SCSS || $arrFile['extension'] == self::LESS)
-				{
-					$strPath = 'assets/' . $strTarget . '/' . str_replace('/', '_', $arrFile['name']) . $this->strMode;
-
-					$objFile = new \File($strPath, true);
-					$objFile->write($autoprefixer->rewrite($this->handleScssLess($content, $arrFile)));
-					$objFile->close();
-
-					$return[] = $strPath;
-				}
-				else
-				{
-					$name = $arrFile['name'];
-
-					// Strip the web/ prefix (see #328)
-					if (strncmp($name, 'web/', 4) === 0)
-					{
-						$name = substr($name, 4);
-					}
-
-					$strPath = 'assets/' . $strTarget . '/' . str_replace('/', '_', $name) . $this->strMode;
-
-					$objFile = new \File($strPath, true);
-					$objFile->write($autoprefixer->rewrite($this->handleCss($content, $arrFile)));
-					$objFile->close();
-
-					// Add the media query (see #7070)
-					if ($arrFile['media'] != '' && $arrFile['media'] != 'all' && strpos($content, '@media') === false)
-					{
-						$strPath .= '" media="' . $arrFile['media'];
-					}
-
-					$return[] = $strPath;
-				}
-			}
-
-			return implode('"><link rel="stylesheet" href="', $return);
-		}
 
 		// Load the existing file
 		if (file_exists(TL_ROOT . '/assets/' . $strTarget . '/' . $strKey . $this->strMode))
@@ -122,7 +182,7 @@ class AutoCombiner extends Combiner
 		}
 
 		// Create the file
-		$objFile = new \File('assets/' . $strTarget . '/' . $strKey . $this->strMode, false);
+		$objFile = new \File('assets/' . $strTarget . '/' . $strKey . $this->strMode);
 		$objFile->truncate();
 		$strFile = '';
 
@@ -152,10 +212,18 @@ class AutoCombiner extends Combiner
 			$strFile .= $content;
 		}
 
-
 		// add vendor prefixes in the combined file
-		$objFile->write($autoprefixer->rewrite($strFile));
+		if ($this->autoprefixer !== null)
+		{
+			$objFile->write($this->autoprefixer->rewrite($strFile));
+		}
+		else
+		{
+			$objFile->write($strFile);
+		}
 		$objFile->close();
+
+		unset($content);
 
 		// Create a gzipped version
 		if (\Config::get('gzipScripts') && function_exists('gzencode'))
