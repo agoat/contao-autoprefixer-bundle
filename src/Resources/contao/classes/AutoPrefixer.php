0ab70881154019f9eeaae4cc7e1e21c404ef66e8
Arne Stappen <meh@agoat.de>
2017-01-26 12:41:04 +0100

    Update AutoPrefixer.php

--- src/Resources/contao/classes/AutoPrefixer.php
+++ src/Resources/contao/classes/AutoPrefixer.php
@@ -5,10 +5,10 @@
  *
  * Copyright (C) 2005-2016 Leo Feyer
  *
- * @package  	 AutoPrefixer
- * @author   	 Arne Stappen
- * @license  	 LGPL-3.0+ 
- * @copyright	 Arne Stappen 2016
+ * @package     AutoPrefixer
+ * @author      Arne Stappen
+ * @license     LGPL-3.0+ 
+ * @copyright   Arne Stappen 2016
  */
 
 namespace Agoat\AutoPrefixer;
@@ -21,47 +21,47 @@ namespace Agoat\AutoPrefixer;
 class AutoPrefixer
 {
     private $browsers = array();
-	
-	public function __construct($browsers = 'last 2 version')
+
+    public function __construct($browsers = 'last 2 version')
     {
-		$this->browsers = $browsers;
+        $this->browsers = $browsers;
 	}
 
     public function rewrite($css)
     {
-		// open wrapper to autoprefixer.js in node.js
+        // open wrapper to autoprefixer.js in node.js
         $nodejs = proc_open
-	(
-			'node ' . \System::getContainer()->getParameter('kernel.root_dir') . '/../vendor/agoat/autoprefixer-bundle/src/Resources/autoprefixer/controller.js',
-			array(array('pipe', 'r'), array('pipe', 'w')),
-			$pipes
-	);
+        (
+            'node ' . \System::getContainer()->getParameter('kernel.root_dir') . '/../vendor/agoat/autoprefixer-bundle/src/Resources/autoprefixer/controller.js',
+            array(array('pipe', 'r'), array('pipe', 'w')),
+            $pipes
+        );
 
         if ($nodejs === false) 
-		{
+        {
             throw new RuntimeException('Could not start node runtime');
         }
 
-		$stdin = array
-		(
-			'css' => $css,
-			'browsers' => $this->browsers,
-			'env' => 'production'
-		);
-		
-		$stdin = json_encode($stdin);
-		
-		// send to node.js
-		fwrite($pipes[0], $stdin);
+        $stdin = array
+        (
+            'css' => $css,
+            'browsers' => $this->browsers,
+            'env' => 'production'
+        );
+        
+        $stdin = json_encode($stdin);
+        
+        // send to node.js
+        fwrite($pipes[0], $stdin);
         fclose($pipes[0]);
 
-		// get from node.js
+        // get from node.js
         $stdout = stream_get_contents($pipes[1]);
         fclose($pipes[1]);
        
-	    // close wrapper in node.js
+        // close wrapper in node.js
         proc_close($nodejs);
-		             
+                     
         return json_decode($stdout, true);
     }
 };
