76378a675c9258303192fa7b71899095d5692eff
Arne Stappen <meh@agoat.de>
2017-01-26 12:27:35 +0100

    Update AutoPrefixer.php

--- src/Resources/contao/classes/AutoPrefixer.php
+++ src/Resources/contao/classes/AutoPrefixer.php
@@ -13,71 +13,55 @@
 
 namespace Agoat\AutoPrefixer;
 
+
 /**
- * Adds prefixes to css
- *
- * Usage:
- *
- *     $combiner = new Autoprefixer();
- *
- *     $combiner->add('css/style.css');
- *     $combiner->add('css/fonts.scss');
- *     $combiner->add('css/print.less');
- *
- *     echo $combiner->getCombinedFile();
- *
+ * Uses node.js to run autoprefixer.js to add prefixes to css
  */
 
 class AutoPrefixer
 {
-
-
     private $browsers = array();
-
-	
 	
 	public function __construct($browsers = 'last 2 version')
     {
 		$this->browsers = $browsers;
 	}
 
-
     public function rewrite($css)
     {
 		// open wrapper to autoprefixer.js in node.js
         $nodejs = proc_open
 		(
-			'node ' . __DIR__ . '/../../autoprefixer/controller.js',
+			'node ' . \System::getContainer()->getParameter('kernel.root_dir') . '/../vendor/agoat/autoprefixer-bundle/src/Resources/autoprefixer/controller.js',
             array(array('pipe', 'r'), array('pipe', 'w')),
             $pipes
         );
-		
+
         if ($nodejs === false) 
 		{
             throw new RuntimeException('Could not start node runtime');
         }
 
-		// send to node.js
 		$stdin = array
 		(
 			'css' => $css,
-			'browsers' => $this->browsers
+			'browsers' => $this->browsers,
+			'env' => 'production'
 		);
 		
 		$stdin = json_encode($stdin);
 		
+		// send to node.js
 		fwrite($pipes[0], $stdin);
         fclose($pipes[0]);
 
 		// get from node.js
         $stdout = stream_get_contents($pipes[1]);
-        $stdout = json_decode($stdout, true);
         fclose($pipes[1]);
        
+	    // close wrapper in node.js
         proc_close($nodejs);
 		             
-        return $stdout;
+        return json_decode($stdout, true);
     }
-    
-   
 };
