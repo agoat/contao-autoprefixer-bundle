e551349932f16e2123b91202ef7b34443c97ba7a
Arne Stappen <meh@agoat.de>
2017-11-21 18:30:44 +0100

    Update tl_layout.php

--- src/Resources/contao/dca/tl_layout.php
+++ src/Resources/contao/dca/tl_layout.php
@@ -19,21 +19,24 @@ $GLOBALS['TL_DCA']['tl_layout']['subpalettes']['autoprefix'] = 'browsers';
 // Fields
 $GLOBALS['TL_DCA']['tl_layout']['fields']['autoprefix'] = array
 (
-	'label'                   => &$GLOBALS['TL_LANG']['tl_layout']['autoprefix'],
-	'exclude'                 => true,
-	'inputType'               => 'checkbox',
-	'eval'                    => array('submitOnChange'=>true, 'tl_class'=>'clr w50 m12'),
-	'sql'                     => "char(1) NOT NULL default ''"
+	'label'         => &$GLOBALS['TL_LANG']['tl_layout']['autoprefix'],
+	'exclude'       => true,
+	'inputType'     => 'checkbox',
+	'eval'          => array('submitOnChange'=>true, 'tl_class'=>'clr w50 m12'),
+	'sql'           => "char(1) NOT NULL default ''"
 );
 $GLOBALS['TL_DCA']['tl_layout']['fields']['browsers'] = array
 (
-	'label'                   => &$GLOBALS['TL_LANG']['tl_layout']['browsers'],
-	'exclude'                 => true,
-	'default'                 => 'last 2 versions',
-	'inputType'               => 'text',
-	'eval'                    => array('maxlength'=>255, 'tl_class'=>'w50'),
-	'save_callback'   	  => array(array('tl_layout_autoprefixer', 'checkBrowserList')),
-	'sql'                     => "varchar(255) NOT NULL default ''"
+	'label'         => &$GLOBALS['TL_LANG']['tl_layout']['browsers'],
+    'default'       => 'last 2 versions',
+    'exclude'       => true,
+	'default'       => 'last 2 versions',
+	'inputType'     => 'text',
+	'eval'          => array('maxlength'=>255, 'tl_class'=>'w50'),
+	'save_callback' => array(
+        array('tl_layout_autoprefixer', 'checkBrowserList')
+    ),
+	'sql'           => "varchar(255) NOT NULL default ''"
 );
 	
 	
@@ -45,13 +48,13 @@ class tl_layout_autoprefixer extends tl_layout
 	/**
 	 * Set to the default value (last 2 versions) if value is empty
 	 *
-	 * @param string Browserslist query
+	 * @param string $value Browserslist query
 	 *
-	 * @return string Browserslist query
+	 * @return string $value Browserslist query
 	 */
-	public function checkBrowserList ($varValue)
+	public function checkBrowserList ($value)
 	{
-		return (empty($varValue)) ? 'last 2 versions' : $varValue;
+		return (empty($value)) ? $GLOBALS['TL_DCA']['tl_layout']['fields']['browsers']['default'] : $value;
 	}
 }
 
