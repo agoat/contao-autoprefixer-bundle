056a4d0e48e25d93c6880257228a4bbff740f4d2
aGoat <mehh@agoat.xyt>
2021-01-20 12:12:33 +0100

    Updated autoprefixer to v 10.2.0.0 from ai/autoprefixer-rails

--- src/Resources/autoprefixer/controller.js
+++ src/Resources/autoprefixer/controller.js
@@ -1,4 +1,4 @@
-var autoprefixer = require('./autoprefixer.js');
+var autoprefixer = require('./autoprefixer.js').autoprefixer;
 var data = '';
 
 process.stdin.on('data', function(chunk) {
@@ -7,7 +7,7 @@ process.stdin.on('data', function(chunk) {
 
 process.stdin.on('end', function() {
 	data = JSON.parse(data);
-	
+
 	try {
 		data.css = autoprefixer.process(data.css, {}, { browsers: data['browsers'], flexbox: data['flex'], grid: data['grid'], remove: data['remove'], supports: data['supports'] }).css;
 	} catch (e) {
